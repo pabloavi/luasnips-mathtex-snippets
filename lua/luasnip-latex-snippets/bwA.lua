e7b5a3cf46c998f1509ad3611bd2b65f17e6bab5
pabloavi <107482263+pabloavi@users.noreply.github.com>
2022-10-20 15:39:52 +0200

    Update Align

--- lua/luasnip-latex-snippets/bwA.lua
+++ lua/luasnip-latex-snippets/bwA.lua
@@ -4,7 +4,7 @@ local t = ls.text_node
 local i = ls.insert_node
 
 local bwA = {
-	s({ trig = "ali", name = "Align" }, { t({ "\\begin{align*}", "\t" }), i(1), t({ "", ".\\end{align*}" }) }),
+	s({ trig = "ali", name = "Align" }, { t({ "\\begin{align*}", "\t" }), i(1), t({ "", ".\\end{align*}" }), i(0) }),
 
 	ls.parser.parse_snippet({ trig = "beg", name = "begin{} / end{}" }, "\\begin{$1}\n\t$0\n\\end{$1}"),
 	ls.parser.parse_snippet({ trig = "case", name = "cases" }, "\\begin{cases}\n\t$1\n\\end{cases}"),
