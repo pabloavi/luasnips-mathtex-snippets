347d9737acac8f3dcac6d7bc8d6b4738525fbcf1
pabloavi <107482263+pabloavi@users.noreply.github.com>
2022-11-16 16:25:57 +0100

    Removed ported

--- lua/luasnip-latex-snippets/bwA.lua
+++ lua/luasnip-latex-snippets/bwA.lua
@@ -3,15 +3,6 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
-local bwA = {
-	s({ trig = "ali", name = "Align" }, { t({ "\\begin{align*}", "\t" }), i(1), t({ "", ".\\end{align*}" }), i(0) }),
-
-	ls.parser.parse_snippet({ trig = "beg", name = "begin{} / end{}" }, "\\begin{$1}\n\t$0\n\\end{$1}"),
-	ls.parser.parse_snippet({ trig = "case", name = "cases" }, "\\begin{cases}\n\t$1\n\\end{cases}"),
-	
---	ls.parser.parse_snippet({ trig = "item", name = "Itemize environment" }, "\\begin{itemize}\n\\item\t$0\n\\end{itemize}"),
-
-
-}
+local bwA = {}
 
 return bwA
