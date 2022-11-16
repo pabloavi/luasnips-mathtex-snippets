a9b74f13779d293f08c528f9e0caede23cfd4e62
pabloavi <107482263+pabloavi@users.noreply.github.com>
2022-11-16 18:59:29 +0100

    Removed ported

--- lua/luasnip-latex-snippets/math_iA_no_backslash.lua
+++ lua/luasnip-latex-snippets/math_iA_no_backslash.lua
@@ -2,10 +2,7 @@ local ls = require("luasnip")
 local with_priority = require("luasnip-latex-snippets.util.utils").with_priority
 
 local math_iA_no_backslash = {
-	ls.parser.parse_snippet({ trig = "sq", name = "\\sqrt{}" }, "\\sqrt{${1:${TM_SELECTED_TEXT}}} $0"),
 
-	ls.parser.parse_snippet({ trig = "inf", name = "\\infty" }, "\\infty"),
-	ls.parser.parse_snippet({ trig = "inn", name = "in " }, "\\in "),
 	ls.parser.parse_snippet({ trig = "SI", name = "SI" }, "\\SI{$1}{$2}"),
 }
 
